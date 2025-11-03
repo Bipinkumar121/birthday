f06454b8bb2c490bf39b3742eed8b5920ca63b09
Bipinkumar121 <bipin.20243076@mnnit.ac.in>
2025-11-03 20:15:31 +0530

    Add files via upload

---
+++ particles.js
@@ -0,0 +1,9 @@
+particlesJS("particles-js", {
+    "particles": {
+        "number": { "value": 100 },
+        "size": { "value": 3 },
+        "move": { "speed": 1 },
+        "line_linked": { "enable": true, "color": "#ff4da6" },
+        "color": { "value": "#ff80bf" }
+    }
+});
